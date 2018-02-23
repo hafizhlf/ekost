f27c84993bb51af4b5cac9681af5ca4a8bad8abd
hafizhlf <aqrashu.asus@gmail.com>
2018-02-23 17:48:58 +0700

    Modifikasi database *penambahan table baru sekaligus ubah table kost

---
+++ database/migrations/2018_02_23_104238_create_kelurahan_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateKelurahanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kelurahan', function (Blueprint $table) {
+            $table->integer('kode_pos');
+            $table->string('kelurahan');
+            $table->integer('kecamatan_id');
+            $table->foreign('kecamatan_id')->references('id')->on('kecamatan')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kelurahan');
+    }
+}
